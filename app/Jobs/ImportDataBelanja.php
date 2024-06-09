afdbf1fc0504a480a5352ccf25efaba6af1bc36e
Ahmad Irham <irham9494h@gmail.com>
2024-06-09 10:21:57 +0800

    import tray catch

--- app/Jobs/ImportDataBelanja.php
+++ app/Jobs/ImportDataBelanja.php
@@ -2,11 +2,13 @@
 
 namespace App\Jobs;
 
+use App\Models\Akun;
 use App\Models\Belanja;
 use App\Models\BidangUrusan;
 use App\Models\BidangUrusanUnitSkpd;
 use App\Models\Kegiatan;
 use App\Models\Program;
+use App\Models\RincianBelanja;
 use App\Models\SubKegiatan;
 use App\Models\UnitSkpd;
 use App\Models\Urusan;
@@ -35,65 +37,76 @@ class ImportDataBelanja implements ShouldQueue
      */
     public function handle(): void
     {
-        // try {
-        //     DB::beginTransaction();
-        foreach ($this->belanjas as $key => $belanja) {
-            $unitSkpd = UnitSkpd::firstOrCreate([
-                'skpd_id' => $this->skpdId,
-                'kode' => str($belanja['KODE SKPD']),
-            ], [
-                'nama' => str($belanja['NAMA SKPD'])->limit(255),
-            ]);
+        try {
+            DB::beginTransaction();
+            foreach ($this->belanjas as $key => $belanja) {
+                $unitSkpd = UnitSkpd::firstOrCreate([
+                    'skpd_id' => $this->skpdId,
+                    'kode' => str($belanja['KODE SKPD']),
+                ], [
+                    'nama' => str($belanja['NAMA SKPD'])->limit(255),
+                ]);
 
-            $urusan = Urusan::firstOrCreate([
-                'kode' => str($belanja['KODE URUSAN']),
-            ], [
-                'nama' => str($belanja['NAMA URUSAN'])->limit(255),
-            ]);
+                $urusan = Urusan::firstOrCreate([
+                    'kode' => str($belanja['KODE URUSAN']),
+                ], [
+                    'nama' => str($belanja['NAMA URUSAN'])->limit(255),
+                ]);
 
-            $bidangUrusan = BidangUrusan::firstOrCreate([
-                'urusan_id' => $urusan->id,
-                'kode' => str($belanja['KODE BIDANG URUSAN']),
-            ], [
-                'nama' => str($belanja['NAMA BIDANG URUSAN'])->limit(255),
-            ]);
+                $bidangUrusan = BidangUrusan::firstOrCreate([
+                    'urusan_id' => $urusan->id,
+                    'kode' => str($belanja['KODE BIDANG URUSAN']),
+                ], [
+                    'nama' => str($belanja['NAMA BIDANG URUSAN'])->limit(255),
+                ]);
 
-            $bidangUrusanUnitSkpd = BidangUrusanUnitSkpd::firstOrCreate([
-                'bidang_urusan_id' => $bidangUrusan->id,
-                'unit_skpd_id' => $unitSkpd->id,
-            ]);
+                $bidangUrusanUnitSkpd = BidangUrusanUnitSkpd::firstOrCreate([
+                    'bidang_urusan_id' => $bidangUrusan->id,
+                    'unit_skpd_id' => $unitSkpd->id,
+                ]);
 
+                $program = Program::firstOrCreate(['kode' => str($belanja['KODE PROGRAM'])], [
+                    'nama' => str($belanja['NAMA PROGRAM'])->limit(255),
+                ]);
 
-            $program = Program::firstOrCreate([
-                'kode' => str($belanja['KODE PROGRAM']),
-            ], [
-                'nama' => str($belanja['NAMA PROGRAM'])->limit(255),
-            ]);
+                $kegiatan = Kegiatan::firstOrCreate([
+                    'program_id' => $program->id,
+                    'kode' => str($belanja['KODE KEGIATAN']),
+                ], [
+                    'nama' => str($belanja['NAMA KEGIATAN'])->limit(255),
+                ]);
 
-            $kegiatan = Kegiatan::firstOrCreate([
-                'program_id' => $program->id,
-                'kode' => str($belanja['KODE KEGIATAN']),
-            ], [
-                'nama' => str($belanja['NAMA KEGIATAN'])->limit(255),
-            ]);
+                $subKegiatan = SubKegiatan::firstOrCreate([
+                    'kegiatan_id' => $kegiatan->id,
+                    'kode' => str($belanja['KODE SUB KEGIATAN']),
+                ], [
+                    'nama' => str($belanja['NAMA SUB KEGIATAN'])->limit(255),
+                ]);
 
-            $subKegiatan = SubKegiatan::firstOrCreate([
-                'kegiatan_id' => $kegiatan->id,
-                'kode' => str($belanja['KODE SUB KEGIATAN']),
-            ], [
-                'nama' => str($belanja['NAMA SUB KEGIATAN'])->limit(255),
-            ]);
+                $akun = Akun::firstOrCreate(['kode' => str($belanja['KODE AKUN'])->before(' ')], [
+                    'nama' => str($belanja['NAMA AKUN'])->limit(255),
+                ]);
 
-            $newBelanja = Belanja::create([
-                'jadwal_penganggaran_id' => $this->jadwalPenganggaranId,
-                'unit_skpd_id' => $unitSkpd->id,
-                'bidang_urusan_id' => $bidangUrusan->id,
-                'sub_kegiatan_id' => $subKegiatan->id,
-            ]);
+                $belanja = Belanja::firstOrCreate([
+                    'jadwal_penganggaran_id' => $this->jadwalPenganggaranId,
+                    'unit_skpd_id' => $unitSkpd->id,
+                    'bidang_urusan_id' => $bidangUrusan->id,
+                    'sub_kegiatan_id' => $subKegiatan->id,
+                ], [
+                    'pagu_murni' => '',
+                    'pagu_validasi' => '',
+                    'total_rincian' => '',
+                ]);
+
+                $rincianBelanja = RincianBelanja::firstOrCreate([
+                    'belanja_id' => $belanja->id,
+                    'rekening_id' => '',
+                    'standar_harga_id' => '',
+                ]);
+            }
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
         }
-        //     DB::commit();
-        // } catch (\Throwable $th) {
-        //     DB::rollBack();
-        // }
     }
 }
